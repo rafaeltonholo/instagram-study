4abaf4dfaafd4102a2dec93457f26641812caba9
Rafael Tonholo <rafaelrtonholo@gmail.com>
2019-05-01 14:18:20 -0300

    domain changes

---
+++ app-src/domain/src/main/java/com/rtonholo/study/instagram/domain/Follower.kt
@@ -0,0 +1,3 @@
+package com.rtonholo.study.instagram.domain
+
+data class Follower(val id: String, val username: String, val fullName: String, val profilePicture: String)
--- app-src/domain/src/main/java/com/rtonholo/study/instagram/domain/User.kt
+++ app-src/domain/src/main/java/com/rtonholo/study/instagram/domain/User.kt
@@ -1,6 +1,7 @@
 package com.rtonholo.study.instagram.domain
 
 data class User(
-    val id: String, val username: String, val fullName: String, val profilePicture: String, val bio: String,
-    val website: String, val isBusiness: Boolean, val follows: Int, val followedBy: Int
+    val id: String, val username: String, val fullName: String, val profilePicture: String,
+    val isBusiness: Boolean, val follows: MutableList<Follower> = mutableListOf(),
+    val followedBy: MutableList<Follower> = mutableListOf(), var bio: String? = null, var website: String?= null
 )
