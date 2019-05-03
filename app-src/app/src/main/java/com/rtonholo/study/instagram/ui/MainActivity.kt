65285d882310d48193d7c69de88aebae8e8ba2ba
Rafael Tonholo <rafaelrtonholo@gmail.com>
2019-05-02 22:00:28 -0300

    added main activity

---
+++ app-src/app/src/main/java/com/rtonholo/study/instagram/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rtonholo.study.instagram.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rtonholo.study.instagram.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
