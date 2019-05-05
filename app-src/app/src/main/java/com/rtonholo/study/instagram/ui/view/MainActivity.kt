71d72d5ef367fd9d2ae78636d59eed58627ecf29
Rafael Tonholo <rafaelrtonholo@gmail.com>
2019-05-04 21:56:10 -0300

    add bottom navigation bar

--- app-src/app/src/main/java/com/rtonholo/study/instagram/ui/view/MainActivity.kt
+++ app-src/app/src/main/java/com/rtonholo/study/instagram/ui/view/MainActivity.kt
@@ -1,13 +1,17 @@
-package com.rtonholo.study.instagram.ui
+package com.rtonholo.study.instagram.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.rtonholo.study.instagram.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nav_bottom.setOnNavigationItemSelectedListener {
+            true
+        }
     }
 }
